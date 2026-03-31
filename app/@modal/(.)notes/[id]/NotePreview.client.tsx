345c1ae62326c0e18b7dc67fe5c414b4bcb62e6c
Byshov <byshov1998@gmail.com>
2026-03-31 19:37:58 +0200

    dix path's

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -2,9 +2,9 @@
 
 import {useParams, useRouter} from "next/navigation";
 import {useQuery} from "@tanstack/react-query";
-import {fetchNoteById} from "@/lib/api/api";
 import Modal from "@/components/Modal/Modal";
 import css from "./NotePreview.module.css"
+import {fetchNoteById} from "@/lib/api/clientApi";
 
 export default function NotePreviewClient(){
     const router = useRouter();
