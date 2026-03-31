8fd84690273da72e2b700139cb7c59dcd8f167c7
Byshov <byshov1998@gmail.com>
2026-03-31 21:09:06 +0200

    sad222

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 
 export type ApiError = AxiosError<{ error: string }>;
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
+const baseURL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
 
 export const nextServer = axios.create({
   baseURL,
